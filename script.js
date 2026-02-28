6aa376903cd471697839191c2a70637800dfaf30
Raja <rajachoudhar7645@gmail.com>
2026-02-28 17:25:13 +0530

    First project upload

---
+++ script.js
@@ -0,0 +1,153 @@
+let tasksDAta = {}
+
+
+const todo = document.querySelector('#todo');
+const inProgress = document.querySelector('#Progress');
+const done = document.querySelector('#done');
+const columns = [todo, inProgress, done];
+let dragElement = null;
+
+
+if (localStorage.getItem("tasks")) {
+
+    const data = JSON.parse(localStorage.getItem("tasks"));
+    console.log(data)
+
+    for (const col in data) {
+        const column = document.querySelector(`#${col}`);
+        data[col].forEach(task => {
+            const div = document.createElement("div")
+
+            // div.classList.add("task")
+            // div.setAttribute("draggable", "true")
+
+            // div.innerHTML = `
+            //     <h2>${task.title}</h2>
+            //     <p>${task.desc}</p>
+            //     <button>Delete</button>
+            // `
+            // column.appendChild(div)
+
+            // div.addEventListener("drag", (e) => {
+            //     dragElement = div;
+            // })
+        })
+    }
+}
+
+
+// console.log(todo, inProgress, done);
+
+const tasks = document.querySelectorAll('.task');
+
+tasks.forEach(task => {
+    task.addEventListener("drag",(e) => {
+        // console.log("dragging", e);
+        dragElement = task;
+    });
+})
+
+// inProgress.addEventListener("dragenter", (e) => {
+//     inProgress.classList.add("hover-over");
+// })
+
+// inProgress.addEventListener("dragleave", (e) => {
+//     inProgress.classList.remove("hover-over");
+// })
+
+function addDragEventsOnColumn(column) {
+    column.addEventListener("dragenter", (e) => {
+        e.preventDefault();
+        column.classList.add("hover-over");
+    })
+
+    column.addEventListener("dragleave", (e) => {
+        e.preventDefault();
+        column.classList.remove("hover-over");
+    })
+
+    column.addEventListener("dragover",(e) => {
+        e.preventDefault();
+    })
+
+    column.addEventListener("drop", (e) => {
+        e.preventDefault();
+
+        console.log("dropped", dragElement, column);
+
+        column.appendChild(dragElement);
+        column.classList.remove("hover-over");
+
+        columns.forEach(col => {
+            const tasks = col.querySelectorAll(".task");
+            const count = col.querySelector(".right");
+
+            count.innerText = tasks.length;
+        })
+    })
+}
+
+addDragEventsOnColumn(todo);
+addDragEventsOnColumn(inProgress);
+addDragEventsOnColumn(done);
+
+
+const toggleModalButton = document.querySelector("#toggle-modal");
+const modalBg = document.querySelector(".modal .bg");
+const modal = document.querySelector(".modal");
+const addTaskButton = document.querySelector("#add-new-task");
+
+
+toggleModalButton.addEventListener("click", () => {
+    modal.classList.toggle("active");
+})
+
+modalBg.addEventListener("click", () => {
+    modal.classList.remove("active");
+})
+
+
+addTaskButton.addEventListener("click",()=>{
+
+    const taskTitle = document.querySelector("#task-title-input").value
+    const taskDesc = document.querySelector("#task-desc-input").value
+
+    const div = document.createElement("div")
+
+    div.classList.add("task")
+    div.setAttribute("draggable", "true")
+
+    div.innerHTML = `
+        <h2>${taskTitle}</h2>
+        <p>${taskDesc}</p>
+        <button>Delete</button>
+
+    `
+    todo.appendChild(div)
+
+    
+
+    columns.forEach(col => {
+        const tasks = col.querySelectorAll(".task");
+        const count = col.querySelector(".right");
+        
+        
+        tasksDAta[ col.id] = Array.from( t => {
+            return {
+                title: t.querySelector("h2").innerText,
+                desc: t.querySelector("p").innerText
+            }
+        })
+
+        localStorage.setItem("tasks",JSON.stringify(tasksDAta));
+        count.innerText = tasks.length;
+    })
+
+
+    div.addEventListener("drag", (e) => {
+        dragElement = div;
+    })
+
+    modal.classList.remove("active")
+    
+})
